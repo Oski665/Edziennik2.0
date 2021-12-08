48431d6875eabd7c2c78e4736d354147678c7c0b
Oskar Betanski <osa76autograf@o2.pl>
2021-12-09 00:52:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/edziennik20/Activity2.kt
@@ -0,0 +1,11 @@
+package com.example.edziennik20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
---
+++ app/src/main/java/com/example/edziennik20/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.edziennik20
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById(R.id.btn_click_me) as Button
+
+        btn_click_me.setOnClickListener{
+            val intent = Intent(this,Activity2::class.java)
+            Toast.makeText(this@MainActivity, "Zalogowano", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+
+//        val listView = findViewById(R.id.listView) as ListView
+//        val butons = arrayOf()
+
+    }
+
+
+}
