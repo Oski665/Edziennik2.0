35bc08239ee3072b61a82e783bc20b38c4bcd947
Dawid <dawnap000@utp.edu.pl>
2022-01-16 00:40:04 +0100

    Logowanie 2.0

--- app/src/main/java/com/example/edziennik20/MainActivity.kt
+++ app/src/main/java/com/example/edziennik20/MainActivity.kt
@@ -8,50 +8,53 @@ import android.widget.EditText
 import android.widget.Toast
 import com.example.edziennik20.ui.database.Database
 import kotlin.properties.Delegates
+import android.R.layout.activity_list_item
+import android.content.DialogInterface
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var database : Database;
-    lateinit var Email: String
-    lateinit var Password: String
+    var email: EditText? = null
+    var password: EditText? = null
+    var btn_click_me: Button? = null
+    var DB: Database? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val password = findViewById<EditText>(R.id.editTextPass)
-        val email = findViewById<EditText>(R.id.editTextEmail)
+        val password = findViewById(R.id.editTextPass) as EditText
+        val email = findViewById(R.id.editTextEmail) as EditText
         val btn_click_me = findViewById(R.id.btn_click_me) as Button
 
 
-        database = Database(this);
-
-        btn_click_me.setOnClickListener{
-
-
-            Email =  email.text.toString();
-            Password =  password.text.toString();
-
-
-            if(Email.equals("")||Password.equals("")){
-                Toast.makeText(this@MainActivity,"Uzupełnij wszystkie pola",Toast.LENGTH_SHORT)
+DB = Database(this)
+        btn_click_me!!.setOnClickListener(object : View.OnClickListener {
+    override fun onClick(view: View?) {
+        val user = email!!.text.toString()
+        val pass = password!!.text.toString()
+        if (user == "" || pass == "") Toast.makeText(
+            this@MainActivity,
+            "Please enter all the fields",
+            Toast.LENGTH_SHORT
+        ).show() else {
+            val checkuserpass: Boolean = DB!!.checkusernamepassword(user, pass)
+            if (checkuserpass == true) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Sign in successfull",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(applicationContext, MainActivity2::class.java)
+                startActivity(intent)
             } else {
-                var checkuserpass : Boolean = database.checkusernamepassword(Email, Password);
-                if(checkuserpass==true){
-                    val intent = Intent(this,MainActivity2::class.java)
-                    Toast.makeText(this@MainActivity, "Zalogowano", Toast.LENGTH_SHORT).show()
-                    startActivity(intent)
-
-                } else {
-                    Toast.makeText(this@MainActivity, "Niepoprawne dane", Toast.LENGTH_SHORT).show()
-                    startActivity(intent)
-
-                }
+                Toast.makeText(
+                    this@MainActivity,
+                    "Invalid Credentials",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-//            val intent = Intent(this,MainActivity2::class.java)
-//            Toast.makeText(this@MainActivity, "Zalogowano", Toast.LENGTH_SHORT).show()
-//            startActivity(intent)
         }
-
     }
-
+})
+}
 }
--- app/src/main/java/com/example/edziennik20/ui/database/Database.kt
+++ app/src/main/java/com/example/edziennik20/ui/database/Database.kt
@@ -1,118 +1,96 @@
-package com.example.edziennik20.ui.database;
+package com.example.edziennik20.ui.database
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteDatabase
+import com.example.edziennik20.ui.database.Database
 
-import androidx.annotation.Nullable;
-
-public class Database extends SQLiteOpenHelper {
-
-    private Context context;
-    public static final String DATABASE_NAME = "E-dziennik";
-    public static final int DATABASE_VERSION = 1;
-    //Encje
-    //Uczniowie
-    public static final String TABLE_NAME_UCZNIOWIE = "Uczniowie";
-    public static final String COLUMN_ID_UCZNIOWIE = "_id_ucznia";
-    public static final String COLUMN_NAME = "imie";
-    public static final String COLUMN_LAST_NAME = "nazwisko";
-    public static final String COLUMN_EMAIL = "email";
-
-    //Uwagi
-    public static final String TABLE_NAME_UWAGI = "Uwagi";
-    public static final String COLUMN_ID_UWAGI = "_id_uwagi";
-    public static final String COLUMN_TRESC = "treść";
-
-
-    //Oceny
-    public static final String TABLE_NAME_OCENY = "Oceny";
-    public static final String COLUMN_ID_OCENY = "_id_oceny";
-    public static final String  COLUMN_OCENA = "Ocena";
-
-    //Sprawdziany
-    public static final String TABLE_NAME_TEST= "Sprawdziany";
-    public static final String COLUMN_ID_TEST = "_id_sprawdzianu";
-    public static final String COLUMN_DATA = "Data";
-
-    //User
-    public static final String TABLE_NAME_USER= "USER";
-    public static final String COLUMN_USERNAME= "Email";
-    public static final String COLUMN_PASSWORD = "Password";
-
-
-
-
-    public Database(Context context) {
-        super(context, "E-dziennik", null, 1);
-    }
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-
-        String query =
-                "CREATE TABLE " + TABLE_NAME_UCZNIOWIE +
-                        "(" + COLUMN_ID_UCZNIOWIE + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_NAME + " TEXT, " +
-                        COLUMN_LAST_NAME + " TEXT, " +
-                        COLUMN_EMAIL + " TEXT);";
-
-
-        String query2 =
-                "CREATE TABLE " + TABLE_NAME_UWAGI +
-                        "(" + COLUMN_ID_UWAGI + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_TRESC + " TEXT);";
-        String query3 =
-                "CREATE TABLE " + TABLE_NAME_OCENY +
-                        "(" + COLUMN_ID_OCENY + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_OCENA + " INTEGER);";
-        String query4 =
-                "CREATE TABLE " + TABLE_NAME_TEST +
-                        "(" + COLUMN_ID_TEST + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        COLUMN_DATA+ " TEXT);";
-        String query5 =
-                "CREATE TABLE " + TABLE_NAME_USER +
-                        "(" + COLUMN_USERNAME + "TEXT PRIMARY KEY, " +
-                        COLUMN_PASSWORD+ " TEXT);";
-        String query6 =
-                "INSERT INTO " + TABLE_NAME_USER + " VALUES('username', 'password')";
-
-
-        db.execSQL(query);
-        db.execSQL(query2);
-        db.execSQL(query3);
-        db.execSQL(query4);
-        db.execSQL(query5);
-        db.execSQL(query6);
-
+class Database(context: Context?) : SQLiteOpenHelper(context, "E-dziennik", null, 1) {
+    private val context: Context? = null
+    override fun onCreate(db: SQLiteDatabase) {
+        val query = "CREATE TABLE " + TABLE_NAME_UCZNIOWIE +
+                "(" + COLUMN_ID_UCZNIOWIE + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_LAST_NAME + " TEXT, " +
+                COLUMN_EMAIL + " TEXT);"
+        val query2 = "CREATE TABLE " + TABLE_NAME_UWAGI +
+                "(" + COLUMN_ID_UWAGI + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TRESC + " TEXT);"
+        val query3 = "CREATE TABLE " + TABLE_NAME_OCENY +
+                "(" + COLUMN_ID_OCENY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_OCENA + " INTEGER);"
+        val query4 = "CREATE TABLE " + TABLE_NAME_TEST +
+                "(" + COLUMN_ID_TEST + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_DATA + " TEXT);"
+        val query5 = "CREATE TABLE " + TABLE_NAME_USER +
+                "(" + COLUMN_USERNAME + " TEXT PRIMARY KEY, " +
+                COLUMN_PASSWORD + " TEXT);"
+        val query6 = "INSERT INTO " + TABLE_NAME_USER + " VALUES('username', 'password')"
+        db.execSQL(query)
+        db.execSQL(query2)
+        db.execSQL(query3)
+        db.execSQL(query4)
+        db.execSQL(query5)
+        db.execSQL(query6)
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_UCZNIOWIE);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_UWAGI);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_OCENY);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_TEST);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_USER);
-
+    override fun onUpgrade(db: SQLiteDatabase, i: Int, i1: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_UCZNIOWIE)
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_UWAGI)
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_OCENY)
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_TEST)
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_USER)
     }
-//    public Boolean insertData(String username, String password){
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues contentValues= new ContentValues();
-//        contentValues.put("username", username);
-//        contentValues.put("password", password);
-//        long result = db.insert("USER", null, contentValues);
-//        if(result==-1) return false;
-//        else
-//            return true;
-//    }
-    public Boolean checkusernamepassword(String Email, String Password){
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("Select * from USER where Email = ? and Password = ?", new String[] {Email,Password});
-        if(cursor.getCount()>0)
-            return true;
-        else
-            return false;
+
+    //    public Boolean insertData(String username, String password){
+    //        SQLiteDatabase db = this.getWritableDatabase();
+    //        ContentValues contentValues= new ContentValues();
+    //        contentValues.put("username", username);
+    //        contentValues.put("password", password);
+    //        long result = db.insert("USER", null, contentValues);
+    //        if(result==-1) return false;
+    //        else
+    //            return true;
+    //    }
+    fun checkusernamepassword(Email: String, Password: String): Boolean {
+        val db = this.writableDatabase
+        val cursor = db.rawQuery(
+            "Select * from "+ TABLE_NAME_USER +" where "+COLUMN_USERNAME+" = ? and "+COLUMN_PASSWORD +" = ?",
+            arrayOf(Email, Password)
+        )
+        return if (cursor.count > 0) true else false
+    }
+
+    companion object {
+        const val DATABASE_NAME = "E-dziennik"
+        const val DATABASE_VERSION = 1
+
+        //Encje
+        //Uczniowie
+        const val TABLE_NAME_UCZNIOWIE = "Uczniowie"
+        const val COLUMN_ID_UCZNIOWIE = "_id_ucznia"
+        const val COLUMN_NAME = "imie"
+        const val COLUMN_LAST_NAME = "nazwisko"
+        const val COLUMN_EMAIL = "email"
+
+        //Uwagi
+        const val TABLE_NAME_UWAGI = "Uwagi"
+        const val COLUMN_ID_UWAGI = "_id_uwagi"
+        const val COLUMN_TRESC = "treść"
+
+        //Oceny
+        const val TABLE_NAME_OCENY = "Oceny"
+        const val COLUMN_ID_OCENY = "_id_oceny"
+        const val COLUMN_OCENA = "Ocena"
+
+        //Sprawdziany
+        const val TABLE_NAME_TEST = "Sprawdziany"
+        const val COLUMN_ID_TEST = "_id_sprawdzianu"
+        const val COLUMN_DATA = "Data"
+
+        //User
+        const val TABLE_NAME_USER = "USER"
+        const val COLUMN_USERNAME = "Email"
+        const val COLUMN_PASSWORD = "Password"
     }
 }
